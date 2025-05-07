f18fdf9c734fcbe83f5eab93a3cce92685351f01
FelipeOrlandoLanzara <felipeorlandolanzara@gmail.com>
2025-05-06 22:05:21 -0300

    plataform n bridge script

--- My project/Assets/Script/Plataform.cs
+++ My project/Assets/Script/Plataform.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Plataform : MonoBehaviour {
 
     public float fallingTime; // tempo que vai demorar para cair quando o player pisar
+    private TargetJoint2D target; // instanciar o target joint para desativar ele depois
+    private TilemapCollider2D tilemapCollider2D; // instanciar para remover os colisores depois
+
     void Start() {
-        
+        target = GetComponent<TargetJoint2D>();
+        tilemapCollider2D = GetComponent<TilemapCollider2D>();
     }
 
     void Update() {
         
     }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Player") { // se colidir com o player
+            Invoke("Falling", fallingTime); // chama a função depois de um tempo
+        }
+    }
+
+    void Falling() { // coloca tudo que vai acontecer depois que o player subir na plataforma
+        target.enabled = false; // desativar o target joint
+        tilemapCollider2D.isTrigger = true; // isso para a plataforma cair e sair do mapa
+    }
+
 }
